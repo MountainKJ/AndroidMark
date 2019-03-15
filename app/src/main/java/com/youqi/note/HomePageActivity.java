0295bcf31b9829b8c8f305c4f844054fbf0c44f5
xudafeng <xudafeng@ireadercity.com>
2019-03-15 10:10:41 +0800

    core recycleview

--- app/src/main/java/com/youqi/note/HomePageActivity.java
+++ app/src/main/java/com/youqi/note/HomePageActivity.java
@@ -3,13 +3,14 @@ package com.youqi.note;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
-
-import com.x.core.rv.WrapRecycleView;
+import android.view.LayoutInflater;
+import android.view.View;
+import com.x.core.rv.WrapRecyclerView;
 import com.youqi.note.model.StringItem;
 
 public class HomePageActivity extends AppCompatActivity {
 
-    private WrapRecycleView mRecycle;
+    private WrapRecyclerView mRecycle;
     private HomePageAdapter adapter;
 
     @Override
@@ -17,14 +18,18 @@ public class HomePageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
         mRecycle = findViewById(R.id.act_hp_ry);
-        LinearLayoutManager llm = new LinearLayoutManager(this);
-        mRecycle.setLayoutManager(llm);
         adapter = new HomePageAdapter(this);
         mRecycle.setAdapter(adapter);
+        addHeaderView();
         for (int i = 0; i < 100; i++) {
             adapter.addItem(new StringItem("123"), null);
         }
         adapter.notifyDataSetChanged();
+    }
+
+    private void addHeaderView() {
+        View headerView = LayoutInflater.from(this).inflate(R.layout.header_hp_layout, mRecycle, false);
+        mRecycle.addHeaderView(headerView);
     }
 
 }
